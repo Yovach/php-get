1b534022114f284c04645aa17a0a1f54ec3e6c52
Yovach <yovach.mail@gmail.com>
2020-07-27 18:39:08 +0200

    feat: source URL has been changed

--- php-get-download.ps1
+++ php-get-download.ps1
@@ -1,10 +1,10 @@
-$SiteAdress = "https://pecl.php.net/package-stats.php";
+$SiteAdress = "https://windows.php.net/downloads/pecl/releases/";
 
 # we fetch the list of all packages from pecl.php.net
 $HttpContent = Invoke-WebRequest -URI $SiteAdress;
 
 # we only take links of packages and get innerText and href properties 
-$links = ($HttpContent.Links | Where-Object {$_.href -like "*/package/*"}) | Select-Object -Property innerText,href;
+$links = ($HttpContent.Links | Where-Object {$_.href -like "*/downloads/pecl/releases/*"}) | Select-Object -Property @{N='name';E={$_.innerText}};
 
-# we export the result to output.json
-ConvertTo-Json $links > .\output.json;
+# we export the result to packages.json
+ConvertTo-Json $links > .\packages.json;
