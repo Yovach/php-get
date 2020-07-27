1cc4b1e8849941ee27ca9c73abc5c3ba6707c8a1
Yovach <yovach.mail@gmail.com>
2020-07-27 21:52:05 +0200

    feat: check if extension is specified

--- php-get.ps1
+++ php-get.ps1
@@ -4,4 +4,17 @@ try {
 } catch {
     Write-Error -Message "An error occured, check if you have PHP installed."
 }
-Write-Output $php_path;
+
+if(!(Test-Path packages.json))
+{
+    Write-Output "Downloading packages.json..";
+    .\php-get-download.ps1;
+}
+# Write-Output $php_path;
+
+$ext = $args[0];
+if (!$ext) {
+    Write-Output "Please indicate an extension to be installed";
+    Break; 
+}
+Write-Output $ext;
